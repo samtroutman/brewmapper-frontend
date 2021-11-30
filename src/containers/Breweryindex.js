db77d5e3b03acfa930b97fc74c7c74a6ea651bba
samtroutman <troutm16@gmail.com>
2021-11-29 23:12:47 -0500

    fix error in brewery index

--- src/containers/Breweryindex.js
+++ src/containers/Breweryindex.js
@@ -1,13 +1,17 @@
 import { useEffect } from "react"
 import { getBreweries } from '../redux/actionCreators'
 import { connect } from 'react-redux'
+import BreweryCard from '../components/BreweryCard'
 
-function BreweryIndex(getBreweries, breweries){
+function BreweryIndex({getBreweries, breweries}){
     
     useEffect(() => breweries.length === 0 && getBreweries(), [breweries])
     
     return(
-        <h1>Breweries</h1>
+        <div>
+            {breweries.map(brewery => <BreweryCard {...brewery} key={brewery.id}/>)}
+        </div>
+        
     )
 }
 
@@ -15,4 +19,4 @@ const mapStateToProps = (state) => {
     return {breweries: state.breweries}
 }
 
-export default connect(mapStateToProps, { getBreweries }) (BreweryIndex);
+export default connect(mapStateToProps, { getBreweries })(BreweryIndex)
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,5 +1,6 @@
 export const getBreweries = () => {
     return dispatch => fetch("http://localhost:3000/breweries")
     .then(res => res.json())
-    .then(breweries => dispatch({type: "GET_BREWERIES", payload: breweries}))
+    .then(breweries => dispatch({type: "GET_BREWERIES", payload: breweries})
+    )
 }
