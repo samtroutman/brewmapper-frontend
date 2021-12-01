1da596f08ed5274ac2c65c2f33cd14cb7c5e87d4
samtroutman <troutm16@gmail.com>
2021-12-01 00:01:10 -0500

    add nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import BreweryIndex from './containers/BreweryIndex';
 import BreweryShow  from  './components/BreweryShow';
+import Nav from './components/Nav';
 import { Switch, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
+      <Nav/>
       <h1>Brewmapper</h1>
       <Switch>
       <Route path="/breweries/:id"><BreweryShow /></Route>
