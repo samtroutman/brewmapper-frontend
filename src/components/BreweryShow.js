8bb3cf2b4853409e886691ab0f7942c04404aa19
samtroutman <troutm16@gmail.com>
2021-11-30 23:30:06 -0500

    add use effect to brewery show

--- src/components/BreweryShow.js
+++ src/components/BreweryShow.js
@@ -1,10 +1,13 @@
 import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux' 
 import { getBrewery } from '../redux/actionCreators'
+import { useEffect } from 'react'
 
-function BreweryShow(props){
+function BreweryShow(getBrewery){
 
     const routeId = useParams().id
+
+    useEffect(() => getBrewery(routeId), [getBrewery, routeId])
 
     return (
         <h1>Brewery Show Page</h1>
