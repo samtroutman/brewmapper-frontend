495d92f3274455c1a9448d5f61144f92175624ec
samtroutman <troutm16@gmail.com>
2021-11-30 23:06:28 -0500

    update brewery card

--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -2,7 +2,7 @@ export default function BreweryCard({name, street, city, state, zip}){
     return (
         <div className="card">
             <h2>{name}</h2>
-            <p>{street}, {city}, {state} {zip}</p>
+            <p>{city}, {state}</p>
         </div>
     )
 }
