fd26173f1b0d77e1048f2b3b19cbbdd3341ab47a
samtroutman <troutm16@gmail.com>
2021-12-01 22:52:47 -0500

    add signup action creator

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -13,3 +13,15 @@ export const getBrewery = (id) => {
 }
 
 export const clearBrewery = () => ({type: "CLEAR_BREWERY"})
+
+export const submitSignup = (user) => {
+    return dispatch => fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+    .then(res => res.json())
+    .then(console.log)
+}
