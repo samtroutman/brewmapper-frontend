014876697c70ffaf161f7f3d8b2fa5dbd0b45359
samtroutman <troutm16@gmail.com>
2021-12-05 23:03:33 -0500

    remove logo from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import BreweryIndex from './containers/BreweryIndex';
 import BreweryShow  from  './components/BreweryShow';
@@ -14,7 +13,7 @@ function App({user, autoLogin}) {
   useEffect(() => localStorage.token && autoLogin(), [autoLogin])
   return (
     <div>
-      <h1>Brewmapper</h1>
+      <h1>Map MI Beers</h1>
       <Nav/>
       { user.username ?
       <Switch>
